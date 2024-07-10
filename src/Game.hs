7036620b8b6911ddb20489f279444b279d42b8cb
schetilt <shetilin.timofey@gmail.com>
2024-07-10 23:49:34 +0200

    Code refactoring 2

--- src/Game.hs
+++ src/Game.hs
@@ -2,25 +2,15 @@ module Game where
 
 import Data.Maybe (Maybe)
 import Utils
+import Types
 
 class Game g where
     newGame :: g
-    makeMove :: g -> Move -> Maybe g
+    validateMove :: g -> Move -> Maybe g
     isGameOver :: g -> Bool
     currentPlayer :: g -> Player
     validMoves :: g -> [Move]
     winner :: g -> Maybe Player
-
-data Player = White | Black deriving (Show, Eq)
-data Piece = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show, Eq)
-data Square = Square Char Int deriving (Show, Eq)
-data Move = Move Square Square (Maybe Piece) deriving (Show, Eq)
-
-data ChessGame = ChessGame {
-    board :: [[Maybe (Player, Piece)]],
-    playerTurn :: Player,
-    moveHistory :: [Move]
-} deriving (Show)
 
 initialBoard :: [[Maybe (Player, Piece)]]
 initialBoard = [[Nothing | _ <- [1..8]] | _ <- [1..8]]  -- Implement the initial chess board setup
@@ -34,18 +24,23 @@ instance Game ChessGame where
         playerTurn = White,
         moveHistory = []
     }
-    makeMove game move =
+    
+    validateMove game move =
         -- Implement chess move logic here
         -- Return Nothing if the move is invalid
         -- Return Just newGame if the move is valid
         undefined
+
     isGameOver game =
         -- Check for checkmate, stalemate, etc.
         False
+    
     currentPlayer game = playerTurn game
+    
     validMoves game =
         -- Generate all valid moves for the current player
         undefined
+    
     winner game
         | isCheckmate game Black = Just White
         | isCheckmate game White = Just Black
--- src/Main.hs
+++ src/Main.hs
@@ -1,54 +1,31 @@
 module Main where
 
 import Game
+import GameVisualizer
+import Types
+import Behaviours
 import System.IO
 
--- Function to display the game state (board and current player)
-displayGame :: ChessGame -> IO ()
-displayGame game = do
-    putStrLn "Current Board:"
-    putStrLn $ show (game :: ChessGame)
-    putStrLn $ "Current Player: " ++ show (currentPlayer game)
+startGameLoop :: ChessGame -> IO ()
+startGameLoop game =
+    iterateGameLoop game makeManualMove makeManualMove  -- Assuming both players are manual for now
 
--- Function to get the user's move
-getUserMove :: IO Move
-getUserMove = do
-    putStrLn "Enter your move (e.g., e2 e4):"
-    moveInput <- getLine
-    let parsedMove = parseMove moveInput
-    case parsedMove of
-        Just move -> return move
-        Nothing   -> do
-            putStrLn "Invalid move format. Please try again."
-            getUserMove
-
--- Function to parse a move from user input
-parseMove :: String -> Maybe Move
-parseMove input =
-    case words input of
-        [from, to] -> Just $ Move (parseSquare from) (parseSquare to) Nothing
-        _ -> Nothing
-
-parseSquare :: String -> Square
-parseSquare [file, rank] = Square file (read [rank])
-parseSquare _ = error "Invalid square format"
-
--- Game loop
-gameLoop :: ChessGame -> IO ()
-gameLoop game = do
+-- Possible signature would be "ChessGame -> (ChessGame -> Move) -> (ChessGame -> Move) -> IO ()"
+-- However, if we get move from IO, we get type mismatch, because decideMove() functions have signature (ChessGame -> IO Move)
+iterateGameLoop game whiteDecideMove blackDecideMove = do
     displayGame game
     if isGameOver game
         then putStrLn $ "Game over! Winner: " ++ show (winner game)
         else do
-            move <- getUserMove
-            case makeMove game move of
+            let currentMakeMoveFunc = if playerTurn game == White then whiteDecideMove else blackDecideMove
+            move <- currentMakeMoveFunc game
+            case validateMove game move of
                 Nothing     -> do
                     putStrLn "Invalid move. Try again."
-                    gameLoop game
-                Just newGame -> gameLoop newGame
+                    iterateGameLoop newGame whiteDecideMove blackDecideMove
+                Just newGame -> iterateGameLoop newGame whiteDecideMove blackDecideMove
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering
     putStrLn "Welcome to the Chess Game!"
-    gameLoop newGame
+    startGameLoop newGame
