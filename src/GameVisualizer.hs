7ec0bd42a6847512f6635103f07beb760a9d147c
schetilt <shetilin.timofey@gmail.com>
2024-07-26 15:28:26 +0200

    Added pretty printign

--- src/GameVisualizer.hs
+++ src/GameVisualizer.hs
@@ -2,20 +2,48 @@ module GameVisualizer where
 
 import Game
 import Types
+import Data.Maybe (fromMaybe)
+import Data.Char
 
--- Function to display the game state (board and current player)
--- Convert a piece to a string
-pieceToString :: Maybe (Player, Piece) -> String
-pieceToString Nothing = "."
-pieceToString (Just (player, piece)) = show player ++ " " ++ show piece
+-- Convert a piece to a string representation
+pieceToChar :: (Player, Piece) -> Char
+pieceToChar (player, piece) = 
+    let pieceChar = case piece of
+            Pawn -> 'P'
+            Knight -> 'N'
+            Bishop -> 'B'
+            Rook -> 'R'
+            Queen -> 'Q'
+            King -> 'K'
+    in if player == White then pieceChar else toLower pieceChar
+
+-- Convert a square to a string
+squareToString :: Maybe (Player, Piece) -> String
+squareToString = maybe "." (return . pieceToChar)
 
 -- Convert a row to a string
 rowToString :: [Maybe (Player, Piece)] -> String
-rowToString row = unwords $ map pieceToString row
+rowToString row = unwords (map squareToString row) ++ "\n"
 
--- Print the game
+-- Convert the entire board to a string
+boardToString :: [[Maybe (Player, Piece)]] -> String
+boardToString board = 
+    let 
+        rows = map rowToString (reverse board)
+        rankLabels = map show (reverse [1..8])
+        labeledRows = zipWith (\label row -> label ++ " " ++ row) rankLabels rows
+        fileLabels = "  a b c d e f g h\n"
+    in unlines labeledRows ++ fileLabels
+
+-- Pretty print the entire chess game
+prettyPrintChessGame :: ChessGame -> String
+prettyPrintChessGame game =
+    let 
+        boardStr = boardToString (board game)
+        turnStr = "Current turn: " ++ show (playerTurn game) ++ "\n"
+        movesStr = "Number of moves: " ++ show (length (moveHistory game)) ++ "\n"
+    in boardStr ++ turnStr ++ movesStr
+
+-- Function to display the game
 displayGame :: ChessGame -> IO ()
-displayGame game = do
-    putStrLn "Current Board:"
-    mapM_ (putStrLn . rowToString) (board game)
-    putStrLn $ "Current Player: " ++ show (playerTurn game)
+displayGame = putStr . prettyPrintChessGame
